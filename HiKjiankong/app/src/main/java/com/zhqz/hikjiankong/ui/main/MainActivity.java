661bc7883e82adc0246e7dad4041f49f16265ce2
guoxuezhu <1174327885@qq.com>
2018-06-01 16:29:03 +0800

    add jar

--- HiKjiankong/app/src/main/java/com/zhqz/hikjiankong/ui/main/MainActivity.java
+++ HiKjiankong/app/src/main/java/com/zhqz/hikjiankong/ui/main/MainActivity.java
@@ -1,17 +1,28 @@
 package com.zhqz.hikjiankong.ui.main;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 import com.zhqz.hikjiankong.R;
 import com.zhqz.hikjiankong.ui.base.BaseActivity;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends BaseActivity implements MainMvpView{
+public class MainActivity extends BaseActivity implements MainMvpView {
 
     @Inject
     MainPresenter mMainPresenter;
+
+    @BindView(R.id.sf_VideoMonitor)
+    SurfaceView m_osurfaceView;
+
+    public final String ADDRESS = "192.168.31.105";
+    public final int PORT = 8000;
+    public final String USER = "admin";
+    public final String PSD = "zhqz58612468";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,7 @@ public class MainActivity extends BaseActivity implements MainMvpView{
         ButterKnife.bind(this);
         activityComponent().inject(this);
         mMainPresenter.attachView(this);
-        mMainPresenter.getchools();
+        //mMainPresenter.getchools();
 
 
     }
